94f0522733e57fe583ceb202c20f3facf7b72e93
selimlouis <selimseyhanoglu@gmail.com>
2020-01-14 11:16:12 +0100

    started the diffuse shading

--- Raytracer.java
+++ Raytracer.java
@@ -8,8 +8,8 @@ import javax.imageio.*;
 public class Raytracer{
 
     public static Ray shootRay(double x, double y, int width, int height, double depth, Matvec camposition){
-        double vecx = 1 - (x / (double) width);
-        double vecy = 1 - (y / (double) height);
+        double vecx = 1 - (x * 2 / (double) width);
+        double vecy = -1 + (y * 2 / (double) height);
         double vecz = depth;
 
         double[] rayvec = {vecx, vecy, vecz};
@@ -18,6 +18,31 @@ public class Raytracer{
         ray.direction.sub(camposition);
         ray.direction.normalize();
         return ray;
+    }
+
+    public static double diffuseColor(Ray surfacenormal, Ray lightray){
+        double result = 0;
+
+        result = surfacenormal.direction.dot(lightray.direction);
+        if(result < 0.2){
+            return 0.2;
+        }
+        return result;
+    }
+
+    public static Ray getLightRay(Matvec lightposition, Ray ray, double t){
+        Matvec startposition = ray.direction.multreturn(t);
+        Matvec lightdirection = lightposition.subreturn(startposition);
+
+        Ray lightray = new Ray(lightdirection, startposition);
+        return lightray;
+    }
+
+    public static Ray getSurfaceNormal(Sphere sphere, Ray ray, double t){
+        Matvec hitposition = ray.direction.multreturn(t);
+        Matvec surfaceNormal = hitposition.subreturn(sphere.position);
+        Ray result = new Ray(surfaceNormal, hitposition);
+        return result;
     }
 
     public static double intersect(Ray ray, Matvec pos, double radius){
@@ -63,8 +88,8 @@ public class Raytracer{
         return t;
     }
 
-    public static int[] raySphereIntersection(Ray ray, ArrayList<Sphere> spheres){
-        int[] result = {0,0,0};
+    public static int[] raySphereIntersection(Ray ray, ArrayList<Sphere> spheres, Matvec lightposition){
+        int[] result = {0,0,0,255};
         double tmin = 99999999999.0;
         Sphere minSphere = null;
         for(int i = 0; i < spheres.size(); i++){
@@ -73,14 +98,21 @@ public class Raytracer{
             if( t <= tmin && t >= 0){
                 tmin = t;
                 minSphere = spheres.get(i);
+                result[0] = minSphere.color[0];
+                result[1] = minSphere.color[1];
+                result[2] = minSphere.color[2];
+                //steps for diffuse color
+                Ray lightray = getLightRay(lightposition, ray, tmin);
+                Ray surfacenormal = getSurfaceNormal(minSphere, ray, tmin);
+                double diffusevalue = diffuseColor(surfacenormal, lightray);
+                //the diffuse value determines the alpha value
+                result[3] = (int) (255 * diffusevalue);
             }
 
         }
         
         
-        if(minSphere != null){
-            result = minSphere.color;
-        }
+        
         return result;
     }
 
@@ -113,9 +145,9 @@ public class Raytracer{
 
     }
 
-    public static void trace(Matvec camposition, ArrayList<Sphere> spheres, int width, int height, double depth, String name){
+    public static void trace(Matvec camposition, ArrayList<Sphere> spheres, int width, int height, double depth, Matvec lightposition, String name){
         //create buffered image
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
         //file object
         File f = null;
@@ -124,12 +156,14 @@ public class Raytracer{
         for(int y = 0; y < height; y++){
             for(int x = 0; x < width; x++){
                 Ray ray = shootRay(x, y, width, height, depth, camposition);
-                int[] color = raySphereIntersection(ray, spheres);
-                int r = (int) ((int) color[0] * (x /(float) width));
+                int[] color = raySphereIntersection(ray, spheres, lightposition);
+                //the color int array adds the alpha value to the last place
+                int a = color[3];
+                int r = color[0];
                 int g = color[1];
-                int b = (int) ((int) color[2] * (y/(float)height));
+                int b = color[2];
                 
-                int rgb = r*(65536) + g*(256) + b;
+                int rgb = a *(256*256*256) + r*(65536) + g*(256) + b;
                 image.setRGB(x, y, rgb);
             }
         }
@@ -152,15 +186,22 @@ public class Raytracer{
 
         //gradient(512, 512, "test");
 
+        double[] lamp = {5,-5, -13};
+        Matvec lampposition = new Matvec(lamp);
         double[] capos = {0,0,1};
-        double[] lipos = {0,0,-2};
-        int[] licolor = {100, 0, 200};
+        double[] lisapos = {0,0,-10};
+        int[] gunthercolor = {20,250,0};
+        double[] guntherpos = {0,0,-15};
+        int[] lisacolor = {100, 0, 100};
         Matvec camposition = new Matvec(capos);
-        Sphere Lisa = new Sphere(lipos, 2, licolor);
+        Sphere Lisa = new Sphere(lisapos, 2, lisacolor);
+
+        Sphere Gunther = new Sphere(guntherpos, 4, gunthercolor);
         ArrayList<Sphere> spheres = new ArrayList<>();
         spheres.add(Lisa);
+        spheres.add(Gunther);
 
-        trace(camposition, spheres, 512, 512, -2, "raytrace");
+        trace(camposition, spheres, 512, 512, -2, lampposition, "raytrace");
 
     }
 }
